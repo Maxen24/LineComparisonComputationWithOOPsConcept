210e49a25d7c1bd1966ea12740b909f49072ddb4
Maxen24 <mshubham242@gmail.com>
2022-09-17 12:42:47 +0530

    Started Line Comparison Computation with Oops UC4

---
+++ src/com/bridgelabz/LineComparisonComputationWithOopsUC4.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author mshub
+ *
+ */
+public class LineComparisonComputationWithOopsUC4 {
+
+	double compareTo(double x1,double x2) {
+		double y1=4;
+		double y2=5;
+		double distance;
+		distance=Math.sqrt(Math.pow(x2-x1,2)+ Math.pow(y2-y1,2));
+		return distance;
+	}
+	
+	LineComparisonComputationWithOopsUC4(){
+		double x1=4;
+		double x2=5;
+		double y1=4;
+		double y2=5;
+	}
+		
+	public static void main(String[] args) {
+		LineComparisonComputationWithOopsUC4 length=new LineComparisonComputationWithOopsUC4();
+			
+		}
+	}
